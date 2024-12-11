444ae4783287280ec6d9a54e1a318de76692d196
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2024-12-11 17:21:00 +0100

    remove profile

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,6 @@ function Home() {
     <div className="flex flex-col w-full h-full items-center justify-start gap-4">
       <p className="text-6xl font-bold">Zoltan Pozsonyi</p>
       <a
-        href="https://profile.zolikon.com/"
-        target="_blank"
-        rel="noreferrer"
-        className="select-none text-4xl hover:text-teal-800 hover:scale-110 transition-all"
-      >
-        Profile
-      </a>
-      <a
         href="https://github.com/Zolikon/"
         target="_blank"
         rel="noreferrer"
